fcee10fdddd9429ac71b32d78c6e87bf3b51ea62
Simon Capriles <simon.capriles@kyohei.com.bo>
2021-07-27 17:47:13 -0400

    Improved apps

--- pacmen.js
+++ pacmen.js
@@ -1,13 +1,12 @@
 let pos = 0;
 const pacArray = [
-    ['./images/PacMan1.png', './images/PacMan2.png'],
-    ['./images/PacMan3.png', './images/PacMan4.png'],
+    ['./PacMan1.png', './PacMan2.png'],
+    ['./PacMan3.png', './PacMan4.png'],
 ];
 let direction = 0;
 var focus = 0;
-const pacMen = []; // This array holds all the pacmen
+const pacMen = [];
 
-// This function returns an object with random values
 function setToRandom(scale) {
     return {
         x: Math.random() * scale,
@@ -15,27 +14,21 @@ function setToRandom(scale) {
     };
 }
 
-// Factory to make a PacMan at a random position with random velocity
 function makePac() {
-    // returns an object with random values scaled {x: 33, y: 21}
-    let velocity = setToRandom(10); // {x:?, y:?}
+    let velocity = setToRandom(10);
     let position = setToRandom(200);
     let direction = {
         x: 0,
         y: 0
     }
-    // Add image to div id = game
     let game = document.getElementById('game');
     let newimg = document.createElement('img');
     newimg.style.position = 'absolute';
     newimg.src = pacArray[0][0];
     newimg.width = 100;
-
-    newimg.style.left = position.x;
-    newimg.style.top = position.y;
+    newimg.style.left = position.x.toString() + "px";
+    newimg.style.top = position.y.toString() + "px";
     game.appendChild(newimg);
-
-    // return details in an object
     return {
         position,
         velocity,
@@ -44,17 +37,15 @@ function makePac() {
     };
 }
 
+setInterval(()=>{focus = (focus + 1) % 2;}, 200);
+
 function update() {
-    // loop over pacmen array and move each one and move image in DOM
     pacMen.forEach((item) => {
         checkCollisions(item);
         item.position.x += item.velocity.x;
         item.position.y += item.velocity.y;
-
-        item.newimg.style.left = item.position.x;
-        item.newimg.style.top = item.position.y;
-
-        focus = (focus + 1) % 2;
+        item.newimg.style.left = item.position.x + "px";
+        item.newimg.style.top = item.position.y + "px";
         item.newimg.src = pacArray[item.direction.x][focus];
     });
     setTimeout(update, 20);
@@ -82,9 +73,5 @@ function checkCollisions(item) {
 }
 
 function makeOne() {
-    pacMen.push(makePac()); // add a new PacMan
-}
-
-if (typeof module !== 'undefined') {
-    module.exports = { checkCollisions, update, pacMen };
+    pacMen.push(makePac());
 }
